e49ca0dc704ebfbb2bdd0f50c9a87593b346fa80
liamjbennett <lbennett@opentable.com>
2014-09-29 09:52:37 +0100

    bring jenkins_job_builder into the module standard

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,28 +1,36 @@
 require 'beaker-rspec/spec_helper'
 require 'beaker-rspec/helpers/serverspec'
 
+
 hosts.each do |host|
-  install_puppet
+  
+  
+	version = ENV['PUPPET_GEM_VERSION']
+	install_puppet(:version => version)
 end
 
-UNSUPPORTED_PLATFORMS = ['Suse','windows','AIX','Solaris']
-
-RSpec.configure do |c|
+Spec.configure do |c|
   proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 
-  c.formatter = :documentation
+	c.formatter = :documentation
 
-  # Configure all nodes in nodeset
-  c.before :suite do
-    puppet_module_install(:source => proj_root, :module_name => 'jenkins_job_builder')
-    hosts.each do |host|
-      on host, puppet('module','install','puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module','install','puppetlabs-inifile'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module','install','rtyler-jenkins'), { :acceptable_exit_codes => [0,1] }
+	c.before :suite do
+		#The gets around a bug where windows can't validate the cert when using https
+		forge_repo = '--module_repository=http://forge.puppetlabs.com'
 
-      on host, 'mkdir /etc/puppet/hiera'
-      scp_to host, File.join(File.dirname(__FILE__),  'hiera.yaml'), '/etc/puppet/hiera.yaml'
-      scp_to host, File.join(File.dirname(__FILE__),  'global.yaml'), '/etc/puppet/hiera/global.yaml'
-    end
+		hosts.each do |host|
+			c.host = host
+      
+			
+
+			path = (File.expand_path(File.dirname(__FILE__)+'/../')).split('/')
+			name = path[path.length-1].split('-')[1]
+
+			copy_module_to(host, :source => proj_root, :module_name => name)
+
+      
+			on host, puppet('module','install', forge_repo, "puppetlabs-stdlib"), { :acceptable_exit_codes => [0,1] }
+	    
+	  end
   end
 end
