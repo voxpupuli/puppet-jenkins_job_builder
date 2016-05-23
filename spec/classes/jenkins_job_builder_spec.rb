297dd4ae73909a68fc0c15ce69a2b8cc24e256b4
Andrew Stangl <andrewstangl@gmail.com>
2016-05-23 21:42:54 +0200

    fix rubocop issues

--- spec/classes/jenkins_job_builder_spec.rb
+++ spec/classes/jenkins_job_builder_spec.rb
@@ -26,47 +26,47 @@ describe 'jenkins_job_builder' do
         it { should contain_file('/etc/jenkins_jobs/jenkins_jobs.ini').with_ensure('present') }
 
         it do
-          should contain_ini_setting('jenkins-jobs user').with({
+          should contain_ini_setting('jenkins-jobs user').with(
             'ensure'  => 'present',
             'path'    => '/etc/jenkins_jobs/jenkins_jobs.ini',
             'section' => 'jenkins',
             'setting' => 'user',
             'value'   => '',
             'require' => 'File[/etc/jenkins_jobs/jenkins_jobs.ini]'
-        })
+          )
         end
 
         it do
-          should contain_ini_setting('jenkins-jobs password').with({
-          'ensure'  => 'present',
-          'path'    => '/etc/jenkins_jobs/jenkins_jobs.ini',
-          'section' => 'jenkins',
-          'setting' => 'password',
-          'value'   => '',
-          'require' => 'File[/etc/jenkins_jobs/jenkins_jobs.ini]'
-        })
+          should contain_ini_setting('jenkins-jobs password').with(
+            'ensure'  => 'present',
+            'path'    => '/etc/jenkins_jobs/jenkins_jobs.ini',
+            'section' => 'jenkins',
+            'setting' => 'password',
+            'value'   => '',
+            'require' => 'File[/etc/jenkins_jobs/jenkins_jobs.ini]'
+          )
         end
 
         it do
-          should contain_ini_setting('jenkins-jobs url').with({
+          should contain_ini_setting('jenkins-jobs url').with(
             'ensure'  => 'present',
             'path'    => '/etc/jenkins_jobs/jenkins_jobs.ini',
             'section' => 'jenkins',
             'setting' => 'url',
             'value'   => 'http://localhost:8080',
             'require' => 'File[/etc/jenkins_jobs/jenkins_jobs.ini]'
-        })
+          )
         end
 
         it do
-          should contain_ini_setting('jenkins-jobs hipchat token').with({
+          should contain_ini_setting('jenkins-jobs hipchat token').with(
             'ensure'  => 'present',
             'path'    => '/etc/jenkins_jobs/jenkins_jobs.ini',
             'section' => 'hipchat',
             'setting' => 'authtoken',
             'value'   => '',
             'require' => 'File[/etc/jenkins_jobs/jenkins_jobs.ini]'
-        })
+          )
         end
       end
     end
