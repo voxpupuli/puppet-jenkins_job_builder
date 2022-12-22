0e055a64b5afdf47c2c4221e1cfb3556b9b8a3df
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-12-22 23:46:31 +0100

    Rubocop autofixes

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -21,22 +21,22 @@ describe 'jenkins_job_builder class' do
 
     # TODO: test ini file
 
-    yaml_content = <<-'YAML'
----
-  - job:
-       description: "This a test job"
-       name: "test"
-       project-type: "freestyle"
-       scm:
-        - git:
-           branches:
-             - "*/master"
-           builders:
-            - shell: "#!/bin/bash -l\necho \"test\"\n"
-           triggers:
-            - pollscm: "*/1 * * * *"
-           url: "git@github.com:opentable/puppet-jenkins_job_builder"
-YAML
+    yaml_content = <<~'YAML'
+      ---
+        - job:
+             description: "This a test job"
+             name: "test"
+             project-type: "freestyle"
+             scm:
+              - git:
+                 branches:
+                   - "*/master"
+                 builders:
+                  - shell: "#!/bin/bash -l\necho \"test\"\n"
+                 triggers:
+                  - pollscm: "*/1 * * * *"
+                 url: "git@github.com:opentable/puppet-jenkins_job_builder"
+    YAML
 
     describe file('/tmp/jenkins-test.yaml') do
       it { is_expected.to be_file }
